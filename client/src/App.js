4f8ee6720418cc9c47ab1c039fe8612ba0ea3132
John Do <v.john.do@outlook.com>
2021-12-12 12:23:22 +1100

    initial commit - proof of concept

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React, { useRef } from "react"
+import './App.scss';
+
+import { Canvas, useFrame } from "@react-three/fiber"
+
+const Box = ( ) => {
+    const mesh = useRef(null)
+    useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+    return (
+        <mesh ref={mesh}>
+            <boxBufferGeometry attach='geometry' args={[1, 1, 1]} />
+            <meshStandardMaterial attach='material' />
+        </mesh>
+    )
+}
+
+function App() {
+    return (
+        <>
+            <Canvas>
+                <Box/>
+            </Canvas>
+        </>
+    );
+}
+
+export default App;
